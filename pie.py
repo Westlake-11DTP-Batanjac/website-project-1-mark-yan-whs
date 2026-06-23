5375e3a32b3ac35d09de816e5c43be15eb40efa0
mark-yan-WHS <cy24295@my.westlake.school.nz>
2026-06-23 12:51:32 +1200

    hi guys i am so excited to learn python, today i just leanrt how to do python lists!! it was so exciting i had so much fun i honeslty cant believe how much fun i had, i even know what a

---
+++ pie.py
@@ -0,0 +1,12 @@
+list=[]
+
+for i in range(5):
+    list.append(int(input()))
+for i in range(5):
+    if list[i] % 2==0:                                 
+        print('even')
+    else:
+        print('odd')
+
+
+
