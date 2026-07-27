529cf71d44fd19eee3bb7433560e7251d248601e
mark-yan-WHS <cy24295@my.westlake.school.nz>
2026-07-28 09:50:53 +1200

    i dont prime nujmbers are kind of hard im nt sure what i am doing this is pretty weird

---
+++ mynewprogramminglanguae.js
@@ -0,0 +1,11 @@
+mynewprogramminglanguae.mkeolr
+pll
+jfo kfkk 
+llll
+p;;;
+a === true
+b ==== false
+kfc = new function
+int doesnot= SVGAnimatedInteger
+sva badge alghoritmh srt
+boom boom = car MediaSourceHandle
--- pie.py
+++ pie.py
@@ -1,13 +1,47 @@
+def funkprime(n):
+    i = 1
+    for i in range(2, n):
+        i += n
+        if n % i == 0:
+            return True
+        else:
+            return False
 
 
-for i in range (100):
-    i += 1
-    if % i == 0:
-        print(i)
-    
+print(funkprime(4))
+
+
+
+
+
+
+
         
-        
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
