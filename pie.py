1d5ff4534b476965ac3b1a66b7f937336717a537
mark-yan-WHS <cy24295@my.westlake.school.nz>
2026-07-31 12:49:56 +1200

    dsadasdeqadadadssda

--- pie.py
+++ pie.py
@@ -1,26 +1,28 @@
 x = 1
 numbers = [1, 2, 5, 7, 31, 13, 24, 25]
 
-
-
-
-
-
-
-
-
-
-
-
-
 def funkprime(n):
-
     for i in range(len(numbers)):
-        
-        if n % numbers[i == 0:
+        if numbers[i] % n == 0:
             return False
 
-    return True
+        return True
+
+
+for i in range(100):
+    print(funkprime(1))
+
+
+
+
+"""
+
+
+
+
+            return False
+
+
 
 
 print(funkprime(x)) 
@@ -31,8 +33,8 @@ print(x)
 
 
 
+"""
 
-        
 
 
 
