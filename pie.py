b688f90c1f1ec04656d33485d4c645672e9e71f1
mark-yan-WHS <cy24295@my.westlake.school.nz>
2026-08-04 10:45:24 +1200

    commit

--- pie.py
+++ pie.py
@@ -1,65 +1,16 @@
-x = 1
-numbers = [1, 2, 5, 7, 31, 13, 24, 25]
-
-def funkprime(n):
-    for i in range(len(numbers)):
-        if numbers[i] % n == 0:
-            return False
-
-        return True
 
 
-for i in range(100):
-    print(funkprime(1))
+try:
+    j = []
+    for i in range(6):
+        p = int(input())
+        j.append(p)
 
-
-
-
-"""
-
-
-
-
-            return False
-
-
-
-
-print(funkprime(x)) 
-print(x)   
-
-
-
-
-
-
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        for i in j:
+            if i < 0:
+                j.remove(i)
+        print(j)
+except: 
+    print('stupid idiot error error')
+    
 
